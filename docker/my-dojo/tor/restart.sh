b26d41193f4109fc2b36316a3e0dd1107819fae5
Josema Arenillas <jochemin@gmail.com>
2019-12-12 08:32:55 +0100

    Recursive permission change

--- docker/my-dojo/tor/restart.sh
+++ docker/my-dojo/tor/restart.sh
@@ -3,7 +3,7 @@ set -e
 
 # Set permissions for bitcoind
 echo "## Set permissions on /var/lib/tor dir ###"
-chmod 750 /var/lib/tor
+chmod -R 750 /var/lib/tor
 
 echo "## Start tor #############################"
 
