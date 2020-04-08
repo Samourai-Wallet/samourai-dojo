7b89a9d0542d22dfe96df0f2ca4fa04c3184ea33
kenshin-samourai <kenshin_samourai@tutanota.com>
2020-04-08 14:33:37 +0200

    modify results returned by dojo.sh onion

--- docker/my-dojo/dojo.sh
+++ docker/my-dojo/dojo.sh
@@ -331,10 +331,8 @@ onion() {
     echo "Explorer hidden service address (v3) = $V3_ADDR_EXPLORER"
   fi
 
-  V2_ADDR=$( docker exec -it tor cat /var/lib/tor/hsv2dojo/hostname )
   V3_ADDR=$( docker exec -it tor cat /var/lib/tor/hsv3dojo/hostname )
-  echo "API hidden service address (v3) = $V3_ADDR"
-  echo "API hidden service address (v2) = $V2_ADDR"
+  echo "Maintenance Tool hidden service address (v3) = $V3_ADDR"
 
   if [ "$BITCOIND_INSTALL" == "on" ]; then
     V2_ADDR_BTCD=$( docker exec -it tor cat /var/lib/tor/hsv2bitcoind/hostname )
